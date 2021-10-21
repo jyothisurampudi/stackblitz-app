3ba5dc8b2a7df8e383446437ba91f1805cea582f
Jyothi <jyothisurampudi54@gmail.com>
2021-10-21 09:44:19 +0530

    initial comments

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Data from './data';
+import AdditionalInfo from './additionalinfo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+class Index extends Component {
+  render() {
+    return (
+      <div>
+        <Data />
+      </div>
+    )
+  }
+}
+
+class MovieInfo extends Component {
+
+  render() {
+    const {id} = this.props.match.params;
+    return (
+      <div>
+        <AdditionalInfo id={id} />
+      </div>
+    )
+  }
+}
+
+class Users extends Component {
+  render() {
+    return (
+      <div>
+        <h1>User</h1>
+      </div>
+    )
+  }
+}
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+ <Router>
+      <div>
+        <Route path="/" exact component={Index} />
+        <Route path="/movieInfo/:id" exact component={MovieInfo} />
+        <Route path="/users/" component={Users} />
+      </div>
+    </Router>
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import {render} from 'react-dom';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+render(
+  
+    <App />,
+  
   document.getElementById('root')
 );
 
